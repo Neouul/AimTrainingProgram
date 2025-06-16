c5c778571f16d0605a60b3ec818af7547f72ae8f
Seok Jieun <seokje7165@naver.com>
2025-06-16 19:32:08 +0900

    추천 감도별 평균 점수 추가 + 약간 개선

--- AimTrainingProgram/ScoreForm.Designer.cs
+++ AimTrainingProgram/ScoreForm.Designer.cs
@@ -42,6 +42,8 @@
             this.comboModeSelect = new System.Windows.Forms.ComboBox();
             this.chartSensitivity = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartDifficulty = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chartSensitivity)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartDifficulty)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +51,7 @@
             // btnHome
             // 
             this.btnHome.Location = new System.Drawing.Point(22, 14);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(2);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(232, 51);
             this.btnHome.TabIndex = 3;
@@ -60,7 +62,7 @@
             // btnBack
             // 
             this.btnBack.Location = new System.Drawing.Point(890, 14);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(226, 51);
             this.btnBack.TabIndex = 2;
@@ -71,7 +73,7 @@
             // btnSetting
             // 
             this.btnSetting.Location = new System.Drawing.Point(694, 14);
-            this.btnSetting.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSetting.Margin = new System.Windows.Forms.Padding(2);
             this.btnSetting.Name = "btnSetting";
             this.btnSetting.Size = new System.Drawing.Size(192, 51);
             this.btnSetting.TabIndex = 10;
@@ -82,7 +84,7 @@
             // btnPlay
             // 
             this.btnPlay.Location = new System.Drawing.Point(258, 12);
-            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.Size = new System.Drawing.Size(235, 53);
             this.btnPlay.TabIndex = 8;
@@ -93,7 +95,7 @@
             // btnAnalyze
             // 
             this.btnAnalyze.Location = new System.Drawing.Point(498, 14);
-            this.btnAnalyze.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAnalyze.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnalyze.Name = "btnAnalyze";
             this.btnAnalyze.Size = new System.Drawing.Size(192, 53);
             this.btnAnalyze.TabIndex = 11;
@@ -125,7 +127,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chartSensitivity.Series.Add(series1);
-            this.chartSensitivity.Size = new System.Drawing.Size(379, 338);
+            this.chartSensitivity.Size = new System.Drawing.Size(893, 338);
             this.chartSensitivity.TabIndex = 16;
             this.chartSensitivity.Text = "chart1";
             // 
@@ -135,7 +137,7 @@
             this.chartDifficulty.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.chartDifficulty.Legends.Add(legend2);
-            this.chartDifficulty.Location = new System.Drawing.Point(559, 181);
+            this.chartDifficulty.Location = new System.Drawing.Point(150, 590);
             this.chartDifficulty.Name = "chartDifficulty";
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
@@ -145,11 +147,33 @@
             this.chartDifficulty.TabIndex = 17;
             this.chartDifficulty.Text = "chart2";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(493, 538);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 30);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "감도별 평균 점수";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(247, 943);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 30);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "난이도별 평균 점수";
+            // 
             // ScoreForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1137, 696);
+            this.ClientSize = new System.Drawing.Size(1597, 986);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.chartDifficulty);
             this.Controls.Add(this.chartSensitivity);
             this.Controls.Add(this.comboModeSelect);
@@ -158,7 +182,7 @@
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.btnHome);
             this.Controls.Add(this.btnBack);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ScoreForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ScoreForm";
@@ -166,6 +190,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chartSensitivity)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartDifficulty)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -179,5 +204,7 @@
         private System.Windows.Forms.ComboBox comboModeSelect;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartSensitivity;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartDifficulty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
