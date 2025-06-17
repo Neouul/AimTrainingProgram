70da4fcffce9a416d33fb4752f16c20f2ec5f889
Eunhyeon <85001394+eunhyeon1@users.noreply.github.com>
2025-06-17 22:32:51 +0900

    문제 해결

--- AimTrainingProgram/TargetingForm.Designer.cs
+++ AimTrainingProgram/TargetingForm.Designer.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿namespace AimTrainingProgram
 {
     partial class TargetingForm
@@ -193,4 +194,217 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnRestart;
     }
+=======
+﻿namespace AimTrainingProgram
+{
+    partial class TargetingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TargetScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.MousePointer = new System.Windows.Forms.PictureBox();
+            this.AimTarget = new System.Windows.Forms.PictureBox();
+            this.comboModeSelect = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MousePointer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AimTarget)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TargetScore
+            // 
+            this.TargetScore.AutoSize = true;
+            this.TargetScore.Font = new System.Drawing.Font("굴림", 20F);
+            this.TargetScore.Location = new System.Drawing.Point(36, 114);
+            this.TargetScore.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.TargetScore.Name = "TargetScore";
+            this.TargetScore.Size = new System.Drawing.Size(330, 54);
+            this.TargetScore.TabIndex = 18;
+            this.TargetScore.Text = "TargetScore";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(562, 94);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(211, 74);
+            this.btnRestart.TabIndex = 19;
+            this.btnRestart.Text = "Replay";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::AimTrainingProgram.Properties.Resources.backicon;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.Location = new System.Drawing.Point(1112, 18);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(214, 75);
+            this.button7.TabIndex = 24;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::AimTrainingProgram.Properties.Resources.homeicon;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Location = new System.Drawing.Point(21, 18);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(216, 75);
+            this.button6.TabIndex = 25;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(244, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(211, 75);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "Play";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(895, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 75);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Settings";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(678, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 75);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "Analyze";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(462, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 75);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "Score";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnScore_Click);
+            // 
+            // MousePointer
+            // 
+            this.MousePointer.Image = global::AimTrainingProgram.Properties.Resources.pointer;
+            this.MousePointer.Location = new System.Drawing.Point(101, 787);
+            this.MousePointer.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.MousePointer.Name = "MousePointer";
+            this.MousePointer.Size = new System.Drawing.Size(29, 47);
+            this.MousePointer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.MousePointer.TabIndex = 17;
+            this.MousePointer.TabStop = false;
+            this.MousePointer.Visible = false;
+            this.MousePointer.Click += new System.EventHandler(this.MousePointer_Click);
+            // 
+            // AimTarget
+            // 
+            this.AimTarget.Image = global::AimTrainingProgram.Properties.Resources.target;
+            this.AimTarget.Location = new System.Drawing.Point(16, 754);
+            this.AimTarget.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.AimTarget.Name = "AimTarget";
+            this.AimTarget.Size = new System.Drawing.Size(81, 80);
+            this.AimTarget.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AimTarget.TabIndex = 16;
+            this.AimTarget.TabStop = false;
+            this.AimTarget.Visible = false;
+            this.AimTarget.Click += new System.EventHandler(this.AimTarget_Click);
+            // 
+            // comboModeSelect
+            // 
+            this.comboModeSelect.FormattingEnabled = true;
+            this.comboModeSelect.Items.AddRange(new object[] {
+            "일반 모드",
+            "약점 분석 모드"});
+            this.comboModeSelect.Location = new System.Drawing.Point(847, 116);
+            this.comboModeSelect.Name = "comboModeSelect";
+            this.comboModeSelect.Size = new System.Drawing.Size(121, 32);
+            this.comboModeSelect.TabIndex = 30;
+            // 
+            // TargetingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 874);
+            this.Controls.Add(this.comboModeSelect);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.TargetScore);
+            this.Controls.Add(this.MousePointer);
+            this.Controls.Add(this.AimTarget);
+            this.Name = "TargetingForm";
+            this.Text = "TargetingForm";
+            this.Load += new System.EventHandler(this.TargetingForm_Load);
+            this.VisibleChanged += new System.EventHandler(this.TargetingForm_VisibleChanged);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TargetingForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SensInScreen);
+            ((System.ComponentModel.ISupportInitialize)(this.MousePointer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AimTarget)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox AimTarget;
+        private System.Windows.Forms.PictureBox MousePointer;
+        private System.Windows.Forms.Label TargetScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboModeSelect;
+    }
+>>>>>>> Stashed changes
 }
--- AimTrainingProgram/TargetingForm.cs
+++ AimTrainingProgram/TargetingForm.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -338,3 +339,455 @@ namespace AimTrainingProgram
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using AimTrainingProgram.Data;
+using AimTrainingProgram.Data.AimTrainingProgram.Data;
+
+namespace AimTrainingProgram
+{
+    public partial class TargetingForm : Form
+    {
+        private Form previousForm;
+
+        private float lolSens = SettingForm.GameSensitivity;
+        private int pcSens = SettingForm.ControlPanelSpeed;
+        private float sensitivityScale;
+
+        private float accumulatedDx = 0;
+        private float accumulatedDy = 0;
+
+        private Point lastMousePos;
+        private Point pointerPosition = Point.Empty;
+        private Bitmap pointerBitmap = Properties.Resources.pointer;
+        private Bitmap targetBitmap = Properties.Resources.target;
+        private Point targetPosition = Point.Empty;
+
+        private Timer targetTimer;
+        private Timer visibilityTimer;
+        private int targetCount = 0;
+        public int score = 0;
+        private Random random = new Random();
+        private bool isTargetHit = false;
+        private bool isTargetVisible = false;
+        private bool isCursorHidden = true;
+        private int targetSize = 50;
+
+        private string resultMessage = "";
+        private Timer resultMessageTimer;
+
+        private List<TargetResult> hitData = new List<TargetResult>();
+        private Point currentTargetCenter;
+        private List<TargetHitRecord> hitRecords = new List<TargetHitRecord>();
+
+        // ComboBox name: comboModeSelect (폼 디자이너에서 추가)
+        // Items: "일반 모드", "약점 분석 모드"
+
+        public TargetingForm(Form previousForm)
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+
+            if (lolSens < 5) sensitivityScale = lolSens;
+            else
+            {
+                if (lolSens <= 10) sensitivityScale = (lolSens / 5) / 32f;
+                else if (lolSens <= 55) sensitivityScale = (lolSens / 5 - 2) / 8f;
+                else sensitivityScale = (lolSens / 5 - 6) / 4f;
+
+                switch (pcSens)
+                {
+                    case 1: sensitivityScale /= 0.03125f; break;
+                    case 2: sensitivityScale /= 0.125f; break;
+                    case 3: sensitivityScale /= 0.25f; break;
+                    case 4: sensitivityScale /= 0.5f; break;
+                    case 5: sensitivityScale /= 0.75f; break;
+                    case 7: sensitivityScale /= 1.5f; break;
+                    case 8: sensitivityScale /= 2.0f; break;
+                    case 9: sensitivityScale /= 2.5f; break;
+                    case 10: sensitivityScale /= 3.0f; break;
+                    case 11: sensitivityScale /= 3.5f; break;
+                }
+            }
+
+            this.previousForm = previousForm;
+        }
+        public TargetingForm() : this(null) { }
+
+        private void TargetingForm_Load(object sender, EventArgs e)
+        {
+            // 콤보박스 항목 직접 추가
+            comboModeSelect.Items.Clear();
+            comboModeSelect.Items.Add("일반 모드");
+            comboModeSelect.Items.Add("약점 분석 모드");
+            comboModeSelect.SelectedIndex = 0;
+            comboModeSelect.SelectedIndexChanged += ComboModeSelect_SelectedIndexChanged;
+
+            btnRestart.Visible = false;
+
+            Point center = new Point(this.ClientSize.Width / 2, this.ClientSize.Height / 2);
+            Cursor.Position = this.PointToScreen(center);
+            pointerPosition = new Point(center.X - 20, center.Y - 20);
+            lastMousePos = center;
+
+            HideCursor();
+            this.Paint += TargetingForm_Paint;
+            this.MouseMove += SensInScreen;
+            this.MouseDown += TargetingForm_MouseDown;
+
+            targetTimer = new Timer();
+            visibilityTimer = new Timer();
+            targetTimer.Tick += ShowTarget;
+            visibilityTimer.Tick += HideTarget;
+
+            switch (SettingForm.SelectedDifficulty)
+            {
+                case SettingForm.Difficulty.Easy:
+                    targetTimer.Interval = 2000;
+                    visibilityTimer.Interval = 1000;
+                    targetSize = 70;
+                    break;
+                case SettingForm.Difficulty.Normal:
+                    targetTimer.Interval = 1000;
+                    visibilityTimer.Interval = 700;
+                    targetSize = 50;
+                    break;
+                case SettingForm.Difficulty.Hard:
+                    targetTimer.Interval = 700;
+                    visibilityTimer.Interval = 800;
+                    targetSize = 20;
+                    break;
+            }
+
+            targetTimer.Start();
+
+            resultMessageTimer = new Timer();
+            resultMessageTimer.Interval = 1000;
+            resultMessageTimer.Tick += (s, ev) =>
+            {
+                resultMessage = "";
+                resultMessageTimer.Stop();
+                Invalidate();
+            };
+        }
+
+        private void TargetingForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            if (isTargetVisible)
+                e.Graphics.DrawImage(targetBitmap, targetPosition.X, targetPosition.Y, targetSize, targetSize);
+
+            int originalW = pointerBitmap.Width;
+            int originalH = pointerBitmap.Height;
+            float scale = 0.8f;
+            int newW = (int)(originalW * scale);
+            int newH = (int)(originalH * scale);
+
+            e.Graphics.DrawImage(pointerBitmap, pointerPosition.X, pointerPosition.Y, newW, newH);
+
+            if (!string.IsNullOrEmpty(resultMessage))
+            {
+                using (Font font = new Font("Segoe UI", 10, FontStyle.Bold))
+                {
+                    Color color = resultMessage == "Perfect!" ? Color.Green : Color.Red;
+
+                    using (Brush brush = new SolidBrush(color))
+                    {
+                        SizeF textSize = e.Graphics.MeasureString(resultMessage, font);
+
+                        PointF msgPosition = new PointF(
+                            targetPosition.X + targetSize / 2 - textSize.Width / 2,
+                            targetPosition.Y - textSize.Height - 2
+                        );
+
+                        e.Graphics.DrawString(resultMessage, font, brush, msgPosition);
+                    }
+                }
+            }
+        }
+
+        // 약점 구역 불러오기
+        private List<(int x, int y)> LoadWeakZones()
+        {
+            var zones = new List<(int, int)>();
+            if (File.Exists("weakzones.txt"))
+            {
+                var data = File.ReadAllText("weakzones.txt");
+                foreach (var part in data.Split(';'))
+                {
+                    var xy = part.Split(',');
+                    if (xy.Length == 2 && int.TryParse(xy[0], out int x) && int.TryParse(xy[1], out int y))
+                        zones.Add((x, y));
+                }
+            }
+            return zones;
+        }
+
+        // 타겟 생성 로직
+        private void ShowTarget(object sender, EventArgs e)
+        {
+            visibilityTimer.Stop();
+
+            if (targetCount >= 10)
+            {
+                targetTimer.Stop();
+                btnRestart.Visible = true;
+                // 게임 종료 시 팝업창으로 점수 안내
+                MessageBox.Show($"게임 종료!\n최종 점수: {score}/10", "결과", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int marginX = this.ClientSize.Width / 4;
+            int marginY = this.ClientSize.Height / 4;
+            int rangeX = this.ClientSize.Width / 2 - 50;
+            int rangeY = this.ClientSize.Height / 2 - 50;
+
+            int x, y;
+            if (comboModeSelect.SelectedItem?.ToString() == "약점 분석 모드")
+            {
+                var weakZones = LoadWeakZones();
+                int gridX = 3, gridY = 2;
+                if (weakZones.Count > 0 && random.NextDouble() < 0.7)
+                {
+                    var zone = weakZones[random.Next(weakZones.Count)];
+                    int zoneWidth = this.ClientSize.Width / gridX;
+                    int zoneHeight = this.ClientSize.Height / gridY;
+                    x = random.Next(zone.x * zoneWidth, (zone.x + 1) * zoneWidth);
+                    y = random.Next(zone.y * zoneHeight, (zone.y + 1) * zoneHeight);
+                }
+                else
+                {
+                    x = random.Next(marginX, marginX + rangeX);
+                    y = random.Next(marginY, marginY + rangeY);
+                }
+            }
+            else // 일반 모드
+            {
+                x = random.Next(marginX, marginX + rangeX);
+                y = random.Next(marginY, marginY + rangeY);
+            }
+
+            targetPosition = new Point(x, y);
+            currentTargetCenter = new Point(x + targetSize / 2, y + targetSize / 2);
+
+            isTargetVisible = true;
+            isTargetHit = false;
+            targetCount++;
+
+            resultMessage = "";
+
+            visibilityTimer.Start();
+            Invalidate();
+        }
+
+        private void HideTarget(object sender, EventArgs e)
+        {
+            isTargetVisible = false;
+            if (!isTargetHit)
+            {
+                hitData.Add(new TargetResult
+                {
+                    Position = currentTargetCenter,
+                    IsHit = false
+                });
+
+                hitRecords.Add(new TargetHitRecord
+                {
+                    Position = targetPosition,
+                    IsHit = false
+                });
+            }
+            visibilityTimer.Stop();
+            Invalidate();
+        }
+
+        private void HideCursor()
+        {
+            Bitmap bmp = new Bitmap(1, 1);
+            IntPtr ptr = bmp.GetHicon();
+            Cursor transparentCursor = new Cursor(ptr);
+            this.Cursor = transparentCursor;
+        }
+
+        public void SensInScreen(object sender, MouseEventArgs e)
+        {
+            int dx = e.X - lastMousePos.X;
+            int dy = e.Y - lastMousePos.Y;
+
+            float scaledDx = dx * sensitivityScale;
+            float scaledDy = dy * sensitivityScale;
+
+            accumulatedDx += scaledDx;
+            accumulatedDy += scaledDy;
+
+            int moveX = (int)accumulatedDx;
+            int moveY = (int)accumulatedDy;
+
+            if (moveX != 0 || moveY != 0)
+            {
+                pointerPosition = new Point(pointerPosition.X + moveX, pointerPosition.Y + moveY);
+                accumulatedDx -= moveX;
+                accumulatedDy -= moveY;
+                Invalidate();
+            }
+
+            lastMousePos = e.Location;
+            HandleCursorVisibility();
+        }
+
+        private void HandleCursorVisibility()
+        {
+            int thresholdY = 50;
+            if (pointerPosition.Y < thresholdY && isCursorHidden)
+            {
+                this.Cursor = Cursors.Default;
+                isCursorHidden = false;
+            }
+            else if (pointerPosition.Y >= thresholdY && !isCursorHidden)
+            {
+                HideCursor();
+                isCursorHidden = true;
+            }
+        }
+
+        private void TargetingForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!isTargetVisible || isTargetHit) return;
+
+            Rectangle targetRect = new Rectangle(targetPosition, new Size(targetSize, targetSize));
+            bool hit = targetRect.Contains(pointerPosition);
+
+            hitData.Add(new TargetResult
+            {
+                Position = currentTargetCenter,
+                IsHit = hit
+            });
+
+            hitRecords.Add(new TargetHitRecord { Position = targetPosition, IsHit = hit });
+
+            if (hit)
+            {
+                score++;
+                TargetScore.Text = $"점수: {score}/10";
+                isTargetHit = true;
+                resultMessage = "Perfect!";
+            }
+            else
+            {
+                resultMessage = "Miss..";
+            }
+
+            resultMessageTimer.Stop();
+            resultMessageTimer.Start();
+            Invalidate();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            SaveScoreData();
+            targetCount = 0;
+            score = 0;
+            isTargetHit = false;
+            TargetScore.Text = "점수: 0/10";
+
+            btnRestart.Visible = false;
+            isTargetVisible = false;
+            visibilityTimer.Stop();
+            targetTimer.Start();
+            Invalidate();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (previousForm != null)
+            {
+                previousForm.Show();
+            }
+            this.Close();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            MainForm home = new MainForm();
+            home.Show();
+            this.Close();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            PlayForm playForm = new PlayForm(this);
+            playForm.Show();
+            this.Hide();
+        }
+
+        private void btnScore_Click(object sender, EventArgs e)
+        {
+            SaveScoreData();
+            ScoreForm scoreForm = new ScoreForm(this);
+            scoreForm.Show();
+            this.Hide();
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            SaveScoreData();
+            AnalyzeForm analyzeForm = new AnalyzeForm(this, hitRecords);
+            analyzeForm.Show();
+            this.Hide();
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            SettingForm settingForm = new SettingForm(this);
+            settingForm.Show();
+            this.Hide();
+        }
+
+        private void MousePointer_Click(object sender, EventArgs e) { }
+        private void AimTarget_Click(object sender, EventArgs e) { }
+
+        private void SaveScoreData()
+        {
+            ScoreData data = new ScoreData
+            {
+                Date = DateTime.Now,
+                Score = score,
+                GameSensitivity = SettingForm.GameSensitivity,
+                ControlPanelSpeed = SettingForm.ControlPanelSpeed,
+                Mode = "Targeting",
+                Difficulty = SettingForm.SelectedDifficulty.ToString()
+            };
+
+            DataManager.SaveScore(data);
+            DataManager.SaveHitRecords(hitRecords);
+        }
+
+        private void TargetingForm_VisibleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible == false)
+            {
+                SaveScoreData();
+            }
+        }
+
+        // 콤보박스 이벤트 핸들러
+        private void ComboModeSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            targetCount = 0;
+            score = 0;
+            btnRestart.Visible = false;
+            isTargetVisible = false;
+            visibilityTimer.Stop();
+            targetTimer.Start();
+            Invalidate();
+        }
+    }
+}
+>>>>>>> Stashed changes
